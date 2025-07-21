3b7442121be3fe0c86b1ca5d5b8604b6e07a3515
Jerome Pinguet <jerome@jerome.cc>
2025-07-21 04:01:00 +0300

    remove clutter

--- auth.ts
+++ auth.ts
@@ -1,6 +1,4 @@
 
-import { post, browserSet, browserGet, setCurrentUser } from '$lib/utils/requestUtils';
-import { variables } from '$lib/utils/constants';
 import { SvelteKitAuth } from "@auth/sveltekit";
 import type { DefaultSession } from "@auth/sveltekit";
 import { AUTH_PROVIDER_ID, AUTH_PROVIDER_NAME, AUTH_PROVIDER_ISSUER, AUTH_PROVIDER_CLIENT_ID, AUTH_PROVIDER_CLIENT_SECRET, AUTH_DEBUG } from '$env/static/private';
@@ -31,37 +29,6 @@ const provider = {
   clientSecret: AUTH_PROVIDER_CLIENT_SECRET // from the provider's dashboard
 };
 
-const handleLogin = async (credential: string) => {
-  if (browserGet('refreshToken')) {
-    localStorage.removeItem('refreshToken');
-  }
-  const url = `${variables.BASE_URI}/api/v2/google?credential=${credential}`;
-  console.log(`url: ${url}`);
-  try {
-    const response = await fetch(
-      url,
-      {
-        credentials: 'include'
-      });
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const json = await response.json();
-    console.log(json);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-  }
-  const setCookieUrl = `${variables.BASE_URI}/api/v2/set-cookie`;
-  let res = await fetch(setCookieUrl);
-  let _json = await res.json();
-  console.log(JSON.stringify(_json));
-  const getCookieUrl = `${variables.BASE_URI}/api/v2/get-cookie`;
-  res = await fetch(getCookieUrl);
-  _json = await res.json();
-  console.log(JSON.stringify(_json));
-};
-
 export const { handle, signIn, signOut } = SvelteKitAuth({
   trustHost:true,
   debug: AUTH_DEBUG=='true' ? true : false,
@@ -69,25 +36,19 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
   callbacks: {
     jwt({ token, user, account }) {
       if (token) {
-        console.log(`token: ${JSON.stringify(token)}`);
+        //console.log(`token: ${JSON.stringify(token)}`);
       }
       if (account) {
-        console.log(`account: ${JSON.stringify(account)}`);
-        const id_token = account.id_token;
-        console.log(`id_token: ${id_token}`)
-        if (id_token) {
-          handleLogin(id_token)
-        }
+        //console.log(`account: ${JSON.stringify(account)}`);
         token.provider = account.provider
       }
       if (user) { // User is available during sign-in
         token.id = user.id
       }
-      console.log(`token:${JSON.stringify(token)}`)
       return token
     },
     session({ session, token }) {
-      console.log(`token: ${JSON.stringify(token)}`);
+      //console.log(`token: ${JSON.stringify(token)}`);
       session.user.userId = token.id as string
       session.user.provider = token.provider as string;
       return session
